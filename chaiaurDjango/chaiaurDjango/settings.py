3d77f6b5afbaff805bfd0aab808c033200736630
riyasehgal codes <riyasehgalcodes@gmail.com>
2026-09-02 22:52:38 +0530

    tailwind-with-django

--- chaiaurDjango/chaiaurDjango/settings.py
+++ chaiaurDjango/chaiaurDjango/settings.py
@@ -39,7 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chai',
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
