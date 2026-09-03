64e1193ffcaeb9c957ac813588cc7bc69735aeb5
riyasehgal codes <riyasehgalcodes@gmail.com>
2026-09-03 21:04:31 +0530

    ORM and models

--- chaiaurDjango/chai/views.py
+++ chaiaurDjango/chai/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import ChaiVariety
+
 # Create your views here.
 
 def all_chai(request):
-    return render(request, 'chai/all_chai.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'chai/all_chai.html', { 'chais': chais })
--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +28,7 @@ urlpatterns = [
     path('chai/', include('chai.urls') ),
     
     
+    
     path("__reload__/", include("django_browser_reload.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
